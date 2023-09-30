ba62735455bbc3b9451cd5ef012987b58592b2dc
GabijaF <145053488+GabijaF@users.noreply.github.com>
2023-09-30 18:10:17 +0300

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -3,167 +3,95 @@
 #include <vector>
 #include <iomanip>
 #include <algorithm>
-#include <cstdlib>
-#include <ctime>
-#include <fstream>
-
-using namespace std;
 
 struct Studentas {
-    string vardas;
-    string pavarde;
-    vector<int> tarpiniai_rezultatai;
-    int egzamino_rezultatas;
+    std::string vardas;
+    std::string pavarde;
+    std::vector<std::string> namuDarbai;
+    int egzaminas;
 };
 
-double GalutinisBalasVidurkis(const vector<int>& tarpiniai, int egzaminas) {
-    if (tarpiniai.empty() || egzaminas > 10) {
-        return -1.0; // Galutinis balas yra '-' jei nėra tarpinių rezultatų arba egzaminas > 10
+double skaiciuotiVidurki(const std::vector<std::string>& pazymiai) {
+    if (pazymiai.empty()) {
+        return 0.0;
     }
-
-    double tarpiniu_vidurkis = 0.0;
-    for (int rezultatas : tarpiniai) {
-        tarpiniu_vidurkis += rezultatas;
+    double suma = 0.0;
+    for (const std::string& pazymys : pazymiai) {
+        suma += std::stod(pazymys); // Konvertuojame į double
     }
-    tarpiniu_vidurkis /= tarpiniai.size();
-
-    return 0.4 * tarpiniu_vidurkis + 0.6 * egzaminas;
+    return suma / pazymiai.size();
 }
 
-double GalutinisBalasMediana(const vector<int>& tarpiniai, int egzaminas) {
-    if (tarpiniai.empty() || egzaminas > 10) {
-        return -1.0; // Galutinis balas yra '-' jei nėra tarpinių rezultatų arba egzaminas > 10
+double skaiciuotiMediana(const std::vector<std::string>& pazymiai) {
+    if (pazymiai.empty()) {
+        return 0.0;
     }
-
-    vector<int> tarpiniai_kopija = tarpiniai;
-    sort(tarpiniai_kopija.begin(), tarpiniai_kopija.end());
-
-    if (tarpiniai_kopija.size() % 2 == 0) {
-        int vidurinio_indeksas1 = tarpiniai_kopija.size() / 2 - 1;
-        int vidurinio_indeksas2 = tarpiniai_kopija.size() / 2;
-        double mediana = (tarpiniai_kopija[vidurinio_indeksas1] + tarpiniai_kopija[vidurinio_indeksas2]) / 2.0;
-        return 0.4 * mediana + 0.6 * egzaminas;
+    std::vector<double> kopija;
+    for (const std::string& pazymys : pazymiai) {
+        kopija.push_back(std::stod(pazymys)); // Konvertuojame į double
+    }
+    std::sort(kopija.begin(), kopija.end());
+    int dydis = kopija.size();
+    if (dydis % 2 == 0) {
+        double vidurinis1 = kopija[dydis / 2 - 1];
+        double vidurinis2 = kopija[dydis / 2];
+        return (vidurinis1 + vidurinis2) / 2.0;
     } else {
-        int vidurinio_indeksas = tarpiniai_kopija.size() / 2;
-        double mediana = tarpiniai_kopija[vidurinio_indeksas];
-        return 0.4 * mediana + 0.6 * egzaminas;
+        return kopija[dydis / 2];
     }
 }
 
-void SpausdintiLentele(const vector<Studentas>& studentai, bool naudotiMediana) {
-    if (naudotiMediana) {
-        cout << left << setw(20) << "Vardas" << setw(20) << "Pavardė" << setw(15) << "Galutinis (Med.)" << endl;
-        cout << "--------------------------------------------------------" << endl;
+double skaiciuotiGalutiniBala(const Studentas& studentas, bool naudotiVidurki) {
+    double namuDarbuBalas = skaiciuotiVidurki(studentas.namuDarbai);
+    double galutinisBalas = (0.4 * namuDarbuBalas + 0.6 * studentas.egzaminas);
+    if (naudotiVidurki) {
+        return galutinisBalas;
     } else {
-        cout << left << setw(20) << "Vardas" << setw(20) << "Pavardė" << setw(15) << "Galutinis (Vid.)" << endl;
-        cout << "-----------------------------------------------------------" << endl;
+        double mediana = skaiciuotiMediana(studentas.namuDarbai);
+        return (0.4 * mediana + 0.6 * studentas.egzaminas);
     }
-
-    for (const Studentas& studentas : studentai) {
-        double galutinis;
-        if (naudotiMediana) {
-            galutinis = GalutinisBalasMediana(studentas.tarpiniai_rezultatai, studentas.egzamino_rezultatas);
-        } else {
-            galutinis = GalutinisBalasVidurkis(studentas.tarpiniai_rezultatai, studentas.egzamino_rezultatas);
-        }
-
-        if (galutinis == -1.0) {
-            cout << left << setw(20) << studentas.vardas << setw(20) << studentas.pavarde << setw(15) << "-" << endl;
-        } else {
-            cout << left << setw(20) << studentas.vardas << setw(20) << studentas.pavarde << setw(15) << fixed << setprecision(2) << galutinis << endl;
-        }
-    }
-}
-
-void NuskaitytiIsFailo(vector<Studentas>& studentai) {
-    ifstream failas("kursiokai.txt"); 
-    
-
-    if (!failas) {
-        cout << "Klaida: Nepavyko atidaryti failo." << endl;
-        return;
-    }
-
-    studentai.clear(); 
-
-    string eilute;
-    while (getline(failas, eilute)) {
-        Studentas studentas;
-        size_t vardo_pozicija = eilute.find_first_of(" ");
-        studentas.vardas = eilute.substr(0, vardo_pozicija);
-        studentas.pavarde = eilute.substr(vardo_pozicija + 1);
-        studentai.push_back(studentas);
-    }
-
-    failas.close();
 }
 
 int main() {
-    vector<Studentas> studentai;
+    std::vector<Studentas> studentai;
+    char pasirinkimas;
 
-    srand(time(NULL)); // Nustatome atsitiktinio generavimo sėklą pagal laiką
+    do {
+        Studentas naujasStudentas;
+        std::cout << "Įveskite studento vardą: ";
+        std::cin >> naujasStudentas.vardas;
+        std::cout << "Įveskite studento pavardę: ";
+        std::cin >> naujasStudentas.pavarde;
 
-    while (true) {
-        Studentas studentas;
-        cout << "Įveskite studento vardą (ENTER, jei norite baigti): ";
-        getline(cin, studentas.vardas);
-
-        if (studentas.vardas.empty()) {
-            break;
-        }
-
-        cout << "Įveskite studento pavardę: ";
-        getline(cin, studentas.pavarde);
-
-        cout << "Generuoti atsitiktinius namų darbų rezultatus? (y/n): ";
-        char generuoti_atstiktinius;
-        cin >> generuoti_atstiktinius;
-        cin.ignore();
-
-        if (generuoti_atstiktinius == 'y' || generuoti_atstiktinius == 'Y') {
-            int nd_sk = rand() % 11; // Sugeneruoti atsitiktinį namų darbų skaičių nuo 0 iki 10
-            for (int i = 0; i < nd_sk; i++) {
-                int pazymys = rand() % 11; // Sugeneruoti atsitiktinį balą nuo 0 iki 10
-                studentas.tarpiniai_rezultatai.push_back(pazymys);
+        std::string pazymys;
+        std::cout << "Įveskite namų darbų rezultatus (baigti su punktu): ";
+        while (std::cin >> pazymys) {
+            if (pazymys == ".") {
+                break; // Nutraukiame įvedimą, jei įvesta taškas
             }
-        } else {
-            cout << "Įveskite studento namų darbų rezultatus (baigti įvedimą su ENTER):" << endl;
-            string eilute;
-            while (true) {
-                getline(cin, eilute);
-                if (eilute.empty()) {
-                    break;
-                }
-                int nd_rezultatas = stoi(eilute);
-                studentas.tarpiniai_rezultatai.push_back(nd_rezultatas);
-            }
+            naujasStudentas.namuDarbai.push_back(pazymys);
         }
 
-        cout << "Generuoti atsitiktinį egzamino balą? (y/n): ";
-        char generuoti_egzamina;
-        cin >> generuoti_egzamina;
-        cin.ignore();
+        std::cout << "Įveskite egzamino rezultatą: ";
+        std::cin >> naujasStudentas.egzaminas;
 
-        if (generuoti_egzamina == 'y' || generuoti_egzamina == 'Y') {
-            studentas.egzamino_rezultatas = rand() % 11; // Sugeneruoti atsitiktinį egzamino balą nuo 0 iki 10
-        } else {
-            cout << "Įveskite studento egzamino rezultatą: ";
-            cin >> studentas.egzamino_rezultatas;
-            cin.ignore();
-        }
+        studentai.push_back(naujasStudentas);
 
-        studentai.push_back(studentas);
+        std::cout << "Ar norite įvesti dar vieno studento duomenis? (T/N): ";
+        std::cin >> pasirinkimas;
+
+    } while (pasirinkimas == 'T' || pasirinkimas == 't');
+
+    std::cout << std::endl;
+    std::cout << std::setw(15) << std::left << "Vardas" << std::setw(15) << std::left << "Pavardė" << std::setw(15) << std::right << "Galutinis (Vid.)" << std::setw(15) << std::right << "Galutinis (Med.)" << std::endl;
+    std::cout << "-----------------------------------------------------------" << std::endl;
+
+    for (const Studentas& studentas : studentai) {
+        double galutinisVidurkiu = skaiciuotiGalutiniBala(studentas, true);
+        double galutinisMediana = skaiciuotiGalutiniBala(studentas, false);
+
+        std::cout << std::setw(15) << std::left << studentas.vardas << std::setw(15) << std::left << studentas.pavarde << std::setw(15) << std::fixed << std::setprecision(2) << std::right << galutinisVidurkiu << std::setw(15) << std::fixed << std::setprecision(2) << std::right << galutinisMediana << std::endl;
     }
-
-    cout << "Pasirinkite balo skaičiavimo būdą:" << endl;
-    cout << "1. Vidurkis" << endl;
-    cout << "2. Mediana" << endl;
-    int pasirinkimas;
-    cin >> pasirinkimas;
-
-    bool naudotiMediana = (pasirinkimas == 2);
-
-    SpausdintiLentele(studentai, naudotiMediana);
 
     return 0;
 }
