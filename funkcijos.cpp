a0074c5b16bc2e3a6760a19a6af0a0c7bdb5f7ac
GabijaF <145053488+GabijaF@users.noreply.github.com>
2023-10-01 19:44:24 +0300

    Update funkcijos.cpp

--- funkcijos.cpp
+++ funkcijos.cpp
@@ -1,32 +1,6 @@
-#include "funkcijos.h"
-#include <algorithm>
-
-double skaiciuotiVidurki(const std::vector<int>& pazymiai) {
-    if (pazymiai.empty()) {
-        return 0.0;
-    }
-    double suma = 0.0;
-    for (int pazymys : pazymiai) {
-        suma += pazymys;
-    }
-    return suma / pazymiai.size();
-}
-
-double skaiciuotiMediana(const std::vector<int>& pazymiai) {
-    if (pazymiai.empty()) {
-        return 0.0;
-    }
-    std::vector<int> kopija = pazymiai;
-    std::sort(kopija.begin(), kopija.end());
-    int dydis = kopija.size();
-    if (dydis % 2 == 0) {
-        int vidurinis1 = kopija[dydis / 2 - 1];
-        int vidurinis2 = kopija[dydis / 2];
-        return (vidurinis1 + vidurinis2) / 2.0;
-    } else {
-        return kopija[dydis / 2];
-    }
-}
+#include "BaloSkaiciavimas.h"
+#include "vidurkis.h"
+#include "mediana.h"
 
 double skaiciuotiGalutiniBala(const Studentas& studentas, bool naudotiVidurki) {
     double NdBalas = skaiciuotiVidurki(studentas.Nd);
@@ -38,3 +12,5 @@ double skaiciuotiGalutiniBala(const Studentas& studentas, bool naudotiVidurki) {
         return (0.4 * mediana + 0.6 * studentas.egz);
     }
 }
+
+
