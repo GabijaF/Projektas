9683cd7e950b5f6c6c1a87682cfdd1c0b58ccb54
GabijaF <145053488+GabijaF@users.noreply.github.com>
2023-10-01 19:41:38 +0300

    Create vidurkis.cpp

---
+++ vidurkis.cpp
@@ -0,0 +1,14 @@
+#include "vidurkis.h"
+
+double skaiciuotiVidurki(const std::vector<int>& pazymiai) {
+    if (pazymiai.empty()) {
+        return 0.0;
+    }
+    double suma = 0.0;
+    for (int pazymys : pazymiai) {
+        suma += pazymys;
+    }
+    return suma / pazymiai.size();
+}
+
+// Papildomos funkcijos susijusios su vidurkiais
