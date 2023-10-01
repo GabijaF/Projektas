47abde2447f9b99e3e594edb5126e89019a75622
GabijaF <145053488+GabijaF@users.noreply.github.com>
2023-10-01 18:51:18 +0300

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -3,52 +3,8 @@
 #include <vector>
 #include <iomanip>
 #include <sstream>
-#include <algorithm>
-
-struct Studentas {
-    std::string vardas;
-    std::string pavarde;
-    std::vector<int> Nd;
-    int egz;
-};
-
-double skaiciuotiVidurki(const std::vector<int>& pazymiai) {
-    if (pazymiai.empty()) {
-        return 0.0;
-    }
-    double suma = 0.0;
-    for (int pazymys : pazymiai) {
-        suma += pazymys;
-    }
-    return suma / pazymiai.size();
-}
-
-double skaiciuotiMediana(const std::vector<int>& pazymiai) {
-    if (pazymiai.empty()) {
-        return 0.0;
-    }
-    std::vector<int> kopija = pazymiai;
-    std::sort(kopija.begin(), kopija.end());
-    int dydis = kopija.size();
-    if (dydis % 2 == 0) {
-        int vidurinis1 = kopija[dydis / 2 - 1];
-        int vidurinis2 = kopija[dydis / 2];
-        return (vidurinis1 + vidurinis2) / 2.0;
-    } else {
-        return kopija[dydis / 2];
-    }
-}
-
-double skaiciuotiGalutiniBala(const Studentas& studentas, bool naudotiVidurki) {
-    double NdBalas = skaiciuotiVidurki(studentas.Nd);
-    double galutinisBalas = (0.4 * NdBalas + 0.6 * studentas.egz);
-    if (naudotiVidurki) {
-        return galutinisBalas;
-    } else {
-        double mediana = skaiciuotiMediana(studentas.Nd);
-        return (0.4 * mediana + 0.6 * studentas.egz);
-    }
-}
+#include "studentas.h"
+#include "funkcijos.h"
 
 int main() {
     std::vector<Studentas> studentai;
@@ -72,7 +28,6 @@ int main() {
         }
 
         if (naujasStudentas.Nd.empty()) {
-            
             continue;
         }
 
@@ -102,3 +57,5 @@ int main() {
 
     return 0;
 }
+
+
