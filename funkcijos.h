a8912f25f9af2f3b9a9152900b6ac003d45a2195
GabijaF <145053488+GabijaF@users.noreply.github.com>
2023-10-01 19:43:46 +0300

    Update funkcijos.h

--- funkcijos.h
+++ funkcijos.h
@@ -1,10 +1,7 @@
 #pragma once
 
-#include <vector>
 #include "studentas.h"
 
-double skaiciuotiVidurki(const std::vector<int>& pazymiai);
-double skaiciuotiMediana(const std::vector<int>& pazymiai);
 double skaiciuotiGalutiniBala(const Studentas& studentas, bool naudotiVidurki);
 
 
